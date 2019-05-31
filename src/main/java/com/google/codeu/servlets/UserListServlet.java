219d95d814ea115367784a08d1b997864db3bf07
Pardis Khodadoustan <pardis.khodadoustan@student.csulb.edu>
2019-05-30 22:56:38 -0700

    step3 & step4: Adding Datastore logic & Adding UserListServlet logic

--- src/main/java/com/google/codeu/servlets/UserListServlet.java
+++ src/main/java/com/google/codeu/servlets/UserListServlet.java
@@ -7,16 +7,40 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.codeu.data.Datastore;
+import com.google.gson.Gson;
+import java.util.Set;
+
 /**
  * Handles fetching all users for the community page.
  */
 @WebServlet("/user-list")
 public class UserListServlet extends HttpServlet{
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws IOException {
+	
+	private Datastore datastore;	
 
-    response.getOutputStream().println("this will be my user list");
-  }
+	@Override
+	public void init() {
+            datastore = new Datastore();
+	}
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	  throws IOException {
+
+//          response.getOutputStream().println("this will be my user list");
+            Set<String> users = datastore.getUsers();
+//          for(String user: users)
+//          {
+//              response.getOutputStream().println(user);
+//          }
+            response.setContentType("application/json");
+            Gson gson = new Gson();
+            String json = gson.toJson(users);
+            response.getOutputStream().println(json);
+
+	}
+
+
     
 }
