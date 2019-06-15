741ac671c4c9873297182df23514b2d0388255ff
Yu Tianyi <chloe_yu@YudeMacBook-Pro-2.local>
2019-06-15 10:45:07 -0700

    change messageServlet to login-status

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -33,7 +33,7 @@ function setPageTitle() {
  * Shows the message form if the user is logged in and viewing their own page.
  */
 function showMessageFormIfViewingSelf() {
-  fetch('/Message')
+  fetch('/login-status')
       .then((response) => {
         return response.json();
       })
