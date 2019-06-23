5dba228c77773c8b2b596db1436787f3022ce1b4
Yu Tianyi <chloe_yu@YudeMacBook-Pro-2.local>
2019-06-23 13:33:56 -0700

    translate

---
+++ src/main/webapp/js/translate-aboutus-loader.js
@@ -0,0 +1,22 @@
+/**
+ * http://usejsdoc.org/
+ */
+      function requestTranslation() {
+        const text = document.getElementById('text').innerHTML;
+        const languageCode = "zh";
+
+        //const resultContainer = document.getElementById('result');
+       // resultContainer.innerText = 'Loading...';
+        const label = document.getElementById('text');
+        const params = new URLSearchParams();
+        params.append('text', text);
+        params.append('languageCode', languageCode);
+        
+        fetch('/translate', {
+          method: 'POST',
+          body: params
+        }).then(response => response.text())
+        .then((translatedMessage) => {
+          label.innerText = translatedMessage;
+        });
+      }
