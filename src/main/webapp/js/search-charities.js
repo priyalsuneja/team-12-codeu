6c659588b8d67d98b85ef0c92ff4f76b57adf5cd
Priyal Suneja <psuneja@ucsd.edu>
2019-07-05 17:52:55 -0700

    Fixed the display for the search and it now works

--- src/main/webapp/js/search-charities.js
+++ src/main/webapp/js/search-charities.js
@@ -11,7 +11,7 @@ function displayCharities() {
         console.log(Object.entries(charities).length);
       if(Object.entries(charities).length === 0) {
         const charityContainer = document.getElementById('display-charities');
-        charityContainer.innerHTML += "Sorry, there are no charities matching this keyword!";
+        charityContainer.innerHTML = "Sorry, there are no charities matching this keyword!";
       }
 
       else {
@@ -20,7 +20,7 @@ function displayCharities() {
 
           charities.forEach((charity) => {
             const charityContainer = document.getElementById('display-charities');
-            charityContainer.innerHTML += charity.name + '&#13;&#10;';
+            charityContainer.innerHTML += charity.name + ', ' + charity.city + '<br>';
           });
       }
     });
